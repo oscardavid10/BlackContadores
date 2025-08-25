2b7b95d77693ae291e998aec81ef526fa1f79197
oscardavid10 <62621473+oscardavid10@users.noreply.github.com>
2025-08-25 16:30:12 -0600

    Update dbConfig.js

--- BlackContadores-API-master/dbase/dbConfig.js
+++ BlackContadores-API-master/dbase/dbConfig.js
@@ -1,19 +1,17 @@
-exports.dbconnection = function ()
-{
-    const connect = 
-    {
-        server: '82.197.95.95',
-        database : 'BLACKCONTADORES',
-        user: 'black_user',
-        password: 'bl4ckc0nt4d0r35.',
-        port: 1433,
-        setTimeout: 30000,
-    
-        options:{
-            encrypt: false,
-            trustedconnection: true,
-        }
-    }
+exports.dbconnection = function () {
+  const connect = {
+    server: "localhost",
+    database: "blackcontadores",
+    user: "SA",
+    password: "TuPassword123!",
+    port: 1433,
+    setTimeout: 30000,
 
-    return connect;
+    options: {
+      encrypt: false,
+      trustedconnection: true,
+    },
+  };
+
+  return connect;
 };
