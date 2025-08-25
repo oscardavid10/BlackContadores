b621e05bdc618f360286d981b669cf32270dc5b9
oscardavid10 <62621473+oscardavid10@users.noreply.github.com>
2025-08-25 16:09:03 -0600

    Update filters.js

--- BlackContadores-Web-master/src/util/filters.js
+++ BlackContadores-Web-master/src/util/filters.js
@@ -1,22 +1,19 @@
-import Vue from 'vue'
-import moment from 'moment'
+import Vue from "vue";
+import moment from "moment";
 
-Vue.filter('formatoMoneda', value => {
-    if (value)
-        return "$" + value.toFixed(2).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
-    else
-        return "$0.00"
+Vue.filter("formatoMoneda", (value) => {
+  const formatted = format(value, 2);
+  return formatted ? `$${formatted}` : "$0.00";
 });
 
-Vue.filter('formatoFecha', function (value) {
-    if (value) {
-        return moment(String(value)).format('DD/MM/YYYY')
-    }
+Vue.filter("formatoFecha", function (value) {
+  if (value) {
+    return moment(String(value)).format("DD/MM/YYYY");
+  }
 });
 
-Vue.filter('formatoPorcentaje', value => {
-    if (value)
-        return value.toFixed(2).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,') + "%"
-    else
-        return "0.00%"
+Vue.filter("formatoPorcentaje", (value) => {
+  if (value)
+    return value.toFixed(2).replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,") + "%";
+  else return "0.00%";
 });
